7b102091da1c331ea41ca9e8fe7b8da7ae7664bc
Concubidated <tbrekke@gmail.com>
2016-03-10 06:06:43 -0800

    Pack Scan: Added Python script to handle the simfile parsing Charts are now stored in the Charts DB table only dance-single notes are parsed, the rest are all zero'd.

--- application/controllers/Scan.php
+++ application/controllers/Scan.php
@@ -124,13 +124,15 @@ class Scan extends CI_Controller {
 				foreach($songfiles as $file){
 					$info = pathinfo($songdir."/".$file);
 					//If sm file exist and not a hidden file.
-					if((strcasecmp(@$info['extension'], 'sm') == '0' || strcasecmp(@$info['extension'],'dwi') == '0' || strcasecmp(@$info['extension'],'ssc') == '0') && $file{0} != '.'){
-						echo $file."\n";
+					if((strcasecmp(@$info['extension'], 'sm') == '0' || strcasecmp(@$info['extension'],'dwi') == '0' 
+					  || strcasecmp(@$info['extension'],'ssc') == '0') && $file{0} != '.'){
+						//echo $file."\n";
 						//Found a simfile that was utf16, hack to handle this.
                                                 //Do not know a better way to do this. Shelling out instead! #bashlyfe
                                                 $cmd = "file -ib \"".$songdir."/".$file."\"";
-						$format = shell_exec($cmd);
-						$format = explode("=",$format)[1];
+						$format = shell_exec(escapeshellcmd($cmd));
+						if(strpos($format,"="))
+							$format = explode("=",$format)[1];
 						if($format == "utf-16le\n"){
                                                         $cmd = 'iconv -f utf16 -t utf8 "'.$songdir."/".$file.'" > "'.$songdir."/".$file.'"';
 							shell_exec($cmd);
@@ -155,95 +157,137 @@ class Scan extends CI_Controller {
 								continue;
 						}
 
-						//Use python to parse the file first, if it fails, then fallback to this...
-						//Also this should be its own function.
+						//Use python to parse the file first, if it fails, then fallback to the php method below...
+						//Set PYTHONPATH becuase I am useing local modules
+						$home = posix_getpwuid(posix_getuid())['dir'];
+						putenv("PYTHONPATH=$home/local/lib/python2.6/site-packages/");
+						$scriptdir = getcwd()."/scripts/";
+						$cmd = 'python '.$scriptdir.'sm_parse.py "'.$songdir.'/'.$file.'"';
+						$results = shell_exec($cmd);
 
-						$artist = preg_grep("/ARTIST/", $fh);
-                                                $title = preg_grep("/TITLE/", $fh);
-                                                $credit = preg_grep("/CREDIT/", $fh);
-						$hash = sha1_file($songdir."/".$file);
-						$subtitle = "''";
-						$titletrans = "''";
-						$subtitletrans = "''";
-						$bannerhash = "";
-
-
-
-						$banner = preg_grep("/BANNER/", $fh);
-						//$notes = array_search('#NOTES:',$fh);
-						//$notes = preg_match("/NOTES\n(.*)\n(.*)\n(.*)\n(.*)/", $fh, $matches);
-
-						//echo $notes."\n";
-						//exit();
-
-
-
-						foreach($banner as $line){
-							$cut = explode(':',$line);
-							if(fnmatch("*#BANNER",$cut[0]))
-								$banner = explode(';',$cut[1])[0];
-							if( !empty($banner) && file_exists($songdir."/".$banner)){
+						if($results != "ERROR"){
+							$results = json_decode($results, TRUE);
+							$song = $results['song'];
+	
+							$artist = $this->db->escape($song['artist']);
+                                                        $title = $this->db->escape($song['title']);
+                                                        $credit = $this->db->escape($song['credit']);
+                                                        $hash = sha1_file($songdir."/".$file);
+                                                        $subtitle = $this->db->escape($song['subtitle']);
+                                                        $titletrans = $this->db->escape($song['titletranslit']);
+                                                        $subtitletrans = $this->db->escape($song['subtitletranslit']);
+                                                        $bannerhash = "";
+							$banner = $this->db->escape($song['banner']);
+							
+                                                        if( !empty($banner) && file_exists($songdir."/".$banner)){							
 								$ext = pathinfo($songdir."/".$banner)['extension'];
 								$bannerhash = md5($songdir."/".$banner.time()).".".$ext;
-								echo $bannerhash."\n";
 								copy($songdir."/".$banner, "static/images/songs/".$bannerhash);
-							} else {
-								echo $songdir."/".$banner." missing";
 							}
 
-						}
 
-						foreach($artist as $line){
-							$cut = explode(':',$line);
-							if(fnmatch("*#ARTIST",$cut[0]))
-								$artist = $this->db->escape(explode(';',$cut[1])[0]);
-						}
+                                                        $sql = "INSERT INTO Songs (`title`, `subtitle`,`titletranslit`,`subtitletranslit`,`artist`,`credit`,`banner`,`hash`)
+                                                                VALUES ($title, $subtitle, $titletrans, $subtitletrans, $artist, $credit,'$bannerhash','$hash')";
+                                                        
+							$query = $this->db->query($sql);
+                                                        $songid = $this->db->insert_id();
+							//$songid = "1";
+                                                        $sql = "INSERT INTO PackSongs (`packid`, `songid`) VALUES ('$packid', '$songid')";
+							$query = $this->db->query($sql);
+							
+							foreach($song['charts'] as $type=>$charts){
+								$type = $this->db->escape($type);
+								foreach ($charts as $key=>$chart){
+									$difficulty = $this->db->escape($key);
+									$meter = $this->db->escape($chart['meter']);
+									$description = $this->db->escape($chart['description']);
+								
+									$holds = is_null(@$chart['notes']['holds']) ? "''" : $this->db->escape(@$chart['notes']['holds']);
+									$jumps = is_null(@$chart['notes']['jumps']) ? "''" : $this->db->escape(@$chart['notes']['jumps']);
+									$mines = is_null(@$chart['notes']['mines']) ? "''" : $this->db->escape(@$chart['notes']['mines']);
+									$rolls = is_null(@$chart['notes']['rolls']) ? "''" : $this->db->escape(@$chart['notes']['rolls']);
+									$taps = is_null(@$chart['notes']['taps']) ? "''" : $this->db->escape(@$chart['notes']['taps']);
+								
+									$sql = "INSERT INTO Charts (`songid`,`type`,`difficulty`,`meter`,`holds`,`jumps`,`mines`,`rolls`,`taps`)
+                                                                                VALUES ('$songid',$type,$difficulty,$meter,$holds,$jumps,$mines,$rolls,$taps)";
+                                                                        //echo "$sql\n";
+									$query = $this->db->query($sql);
+								}//end for each chart
+							}//end foreach type of chart
 
-						foreach($title as $line){
-							//Had to add strip_tags cause yolomania pack is stupid
-							$cut = explode(':', strip_tags($line));
-							//print_r($cut);
-							if(fnmatch("*#TITLE",$cut[0]))
-								$title = $this->db->escape(explode(";",$cut[1])[0]);
-							if(fnmatch("*#SUBTITLE",$cut[0]))
-								$subtitle = $this->db->escape(explode(';',$cut[1])[0]);
-						        if(fnmatch("*#TITLETRANSLIT",$cut[0]))
-                                                                $titletrans = $this->db->escape(explode(';',$cut[1])[0]);
-                                                        if(fnmatch("*#SUBTITLETRANSLIT",$cut[0]))
-                                                                $subtitletrans = $this->db->escape(explode(';',$cut[1])[0]);
-						}
+						} else {
+							$artist = preg_grep("/ARTIST/", $fh);
+							$title = preg_grep("/TITLE/", $fh);
+							$credit = preg_grep("/CREDIT/", $fh);
+							$hash = sha1_file($songdir."/".$file);
+							$subtitle = "''";
+							$titletrans = "''";
+							$subtitletrans = "''";
+							$bannerhash = "";
+							$banner = preg_grep("/BANNER/", $fh);
 
-						if(!empty($credit)){
-                                                	foreach($credit as $line){
-                                                        	$cut = explode(':',$line);
-                                                        	if(fnmatch("*#CREDIT",$cut[0]))
-                                                                	$credit = $this->db->escape(explode(';',$cut[1])[0]);
-                                                	}
-						} else
-							$credit = "''";
+							foreach($banner as $line){
+								$cut = explode(':',$line);
+								if(fnmatch("*#BANNER",$cut[0]))
+									$banner = explode(';',$cut[1])[0];
+								if( !empty($banner) && file_exists($songdir."/".$banner)){
+									$ext = pathinfo($songdir."/".$banner)['extension'];
+									$bannerhash = md5($songdir."/".$banner.time()).".".$ext;
+									echo $bannerhash."\n";
+									copy($songdir."/".$banner, "static/images/songs/".$bannerhash);
+								} else {
+									echo $songdir."/".$banner." missing";
+								}
+							}//end banner parser
 
-						//Cause sometimes people have typos in simfiles.
-						if(is_array($credit))
-							$credit = "''";
+							foreach($artist as $line){
+								$cut = explode(':',$line);
+								if(fnmatch("*#ARTIST",$cut[0]))
+									$artist = $this->db->escape(explode(';',$cut[1])[0]);
+							}
 
-						//if(is_array($title))
-						//echo $title."\n";
-						$sql = "INSERT INTO Songs (`title`, `subtitle`,`titletranslit`,`subtitletranslit`,`artist`,`credit`,`banner`,`hash`)
-							VALUES ($title, $subtitle, $titletrans, $subtitletrans, $artist, $credit,'$bannerhash','$hash')";
-						$query = $this->db->query($sql);
-						$songid = $this->db->insert_id();
+							foreach($title as $line){
+								//Had to add strip_tags cause yolomania pack is stupid
+								$cut = explode(':', strip_tags($line));
+								//print_r($cut);
+								if(fnmatch("*#TITLE",$cut[0]))
+									$title = $this->db->escape(explode(";",$cut[1])[0]);
+								if(fnmatch("*#SUBTITLE",$cut[0]))
+									$subtitle = $this->db->escape(explode(';',$cut[1])[0]);
+								if(fnmatch("*#TITLETRANSLIT",$cut[0]))
+									$titletrans = $this->db->escape(explode(';',$cut[1])[0]);
+								if(fnmatch("*#SUBTITLETRANSLIT",$cut[0]))
+									$subtitletrans = $this->db->escape(explode(';',$cut[1])[0]);
+							}//end title(s) parser
 
-						$sql = "INSERT INTO PackSongs (`packid`, `songid`) VALUES ('$packid', '$songid')";
-                                                $query = $this->db->query($sql);
+							if(!empty($credit)){
+								foreach($credit as $line){
+									$cut = explode(':',$line);
+									if(fnmatch("*#CREDIT",$cut[0]))
+										$credit = $this->db->escape(explode(';',$cut[1])[0]);
+								}
+							} else
+								$credit = "''";
 
+							//Cause sometimes people have typos in simfiles.
+							if(is_array($credit))
+								$credit = "''";
+
+							//if(is_array($title))
+							//echo $title."\n";
+							$sql = "INSERT INTO Songs (`title`, `subtitle`,`titletranslit`,`subtitletranslit`,`artist`,`credit`,`banner`,`hash`)
+								VALUES ($title, $subtitle, $titletrans, $subtitletrans, $artist, $credit,'$bannerhash','$hash')";
+							$query = $this->db->query($sql);
+							$songid = $this->db->insert_id();
+
+							$sql = "INSERT INTO PackSongs (`packid`, `songid`) VALUES ('$packid', '$songid')";
+							$query = $this->db->query($sql);
+							
+						}//end else
+						
 					}//if sm file
 
 				}//end foreach song
-
-
-				//echo "<pre>";
-				//print_r($songfiles);
-				//echo "</pre>";
 			}
 
 		}//end foreach song
---
+++ scripts/sm_parse.py
@@ -0,0 +1,120 @@
+import sys
+import json
+from simfile import *
+from pprint import pprint
+
+filename = sys.argv[1]
+
+simfile = {}
+simfile['song'] = {}
+
+try:
+	sim = Simfile(filename)
+except:
+	print "ERROR"
+	#print "ERROR"
+	exit()
+try:
+	simfile['song']['title'] = sim['TITLE']
+except:
+	simfile['song']['title'] = ""
+
+try:
+	simfile['song']['artist'] = sim['ARTIST']
+except:
+	simfile['song']['artist'] = ""
+
+try:
+	simfile['song']['banner'] = sim['BANNER']
+except:
+	simfile['song']['banner'] = ""
+
+try:
+	simfile['song']['background'] = sim['BACKGROUND']
+except:
+	simfile['song']['background'] = ""
+
+try:
+	simfile['song']['credit'] = sim['CREDIT']
+except:
+	simfile['song']['credit'] = ""
+
+try:
+	simfile['song']['subtitle'] = sim['SUBTITLE']
+except:
+	simfile['song']['subtitle'] = ""
+
+try:
+	simfile['song']['artisttranslit'] = sim['ARTISTTRANSLIT']
+except:
+	simfile['song']['artisttranslit'] = ""
+
+try:
+	simfile['song']['titletranslit'] = sim['TITLETRANSLIT']
+except:
+	simfile['song']['titletranslit'] = ""
+
+try:
+	simfile['song']['subtitletranslit'] = sim['SUBTITLETRANSLIT']
+except:
+	simfile['song']['subtitletranslit'] = ""
+
+try:
+	simfile['song']['bgchanges'] = sim['BGCHANGES']
+except:
+	simfile['bgchanges'] = ""
+
+try:
+	simfile['song']['fgchanges'] = sim['FGCHANGES']
+except:
+	simfile['song']['fgchanges'] = ""
+
+simfile['song']['charts'] = {}
+
+for chart in sim.charts:
+	type = str(chart.stepstype)
+	if not simfile['song']['charts'].get(type):
+        	simfile['song']['charts'][type] = {}
+	simfile['song']['charts'][type][chart.difficulty] = {}
+        simfile['song']['charts'][type][chart.difficulty]['meter'] = chart.meter
+        simfile['song']['charts'][type][chart.difficulty]['description'] = chart.description
+
+	if chart.stepstype in 'dance-single':
+		simfile['song']['charts'][type][chart.difficulty]['notes'] = {}
+		taps = 0
+		holds = 0
+		jumps = 0
+		mines = 0
+		rolls = 0
+		for notes in chart.notes:
+			if notes[1] in ("1001","0110","1100","0011", "1010", "0101"):
+				jumps+=1
+				taps+=2			
+			elif notes[1] in ("2002","0220","2200","0022", "2020", "0202"):
+				jumps+=1
+				holds+=2
+				taps+=2
+			elif notes[1] in ("1002","0120","1200","0012", "1020", "0102", "2001","0210","2100","0021", "2010", "0201"):
+				jumps+=1
+				holds+=1
+				taps+=2
+			else:
+				for l in list(notes[1]):
+					if l in ("M", "m"):
+						mines+=1
+					elif int(l) == 1:
+						taps+=1
+					elif int(l) == 2:
+        	                                holds+=1
+						taps+=1
+					elif int(l) == 4:
+						rolls+=1
+						taps+=1
+					#else:
+		simfile['song']['charts'][type][chart.difficulty]['notes']['taps'] = taps
+                simfile['song']['charts'][type][chart.difficulty]['notes']['holds'] = holds
+                simfile['song']['charts'][type][chart.difficulty]['notes']['jumps'] = jumps
+                simfile['song']['charts'][type][chart.difficulty]['notes']['mines'] = mines
+                simfile['song']['charts'][type][chart.difficulty]['notes']['rolls'] = rolls
+
+print json.dumps(simfile, sort_keys=True, indent=4)
